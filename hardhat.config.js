c4490651066d12ee6104648d4f0d8e5b9ada9378
rainbowginger <114979413+rainbowginger@users.noreply.github.com>
2023-04-29 17:38:00 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-// const RPC_URL = process.env.SEPOLIA_RPC_URL;
-// const PVT_KEY = process.env.SEPOLIA_PVT_KEY;
-// const CHAIN_ID = process.env.SEPOLIA_CHAIN_ID;
-// const API_KEY = process.env.ETHERSCAN_API_KEY;
+const RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PVT_KEY = process.env.SEPOLIA_PVT_KEY;
+const CHAIN_ID = process.env.SEPOLIA_CHAIN_ID;
+
 // commented section is working
-const RPC_URL =
-    "https://eth-sepolia.g.alchemy.com/v2/SYu8cyWG0EOfcfZVehXUoPMXW91om6Hd";
-const PVT_KEY =
-    "privatekey";
-const CHAIN_ID = 11155111;
-const API_KEY = "91Y7P3IIZBKAACGWTHB93ZSG62ENS3AFQX";
+// const RPC_URL =
+//     "https://eth-sepolia.g.alchemy.com/v2/SYu8cyWG0EOfcfZVehXUoPMXW91om6Hd";
+// const PVT_KEY =
+//     "privatekey";
+// const CHAIN_ID = 11155111;
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -22,9 +22,7 @@ module.exports = {
         },
     },
     solidity: "0.8.18",
-    etherscan: {
-        apiKey: API_KEY,
-    },
+    
 };
 
 task("accounts", "Prints the list of accounts", async () => {
