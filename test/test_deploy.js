9d53df6bfb9e6d708b6df38d5557623c55fdb878
rainbowginger <rvhj@duck.com>
2023-05-05 19:05:44 +0530

    test

---
+++ test/test_deploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+// const { describe, beforeEach, it } = require("node:test");
+const { assert, expect } = require("chai");
+describe("SimpleStorage", function () {
+    let storagefactory, Simplefactory;
+    beforeEach(async function () {
+        storagefactory = await ethers.getContractFactory("SimpleStorage");
+        Simplefactory = await storagefactory.deploy();
+    });
+    it("Should start with 0", async function () {
+        const defaultvalue = await Simplefactory.retrieve();
+        const expectedValue = 0;
+        assert.equal(defaultvalue.toString(), expectedValue);
+    });
+    it("input value equal to stored value", async function () {
+        const storing = "55";
+        const retireve = await Simplefactory.store(storing);
+        assert.equal(storing.toString(), await Simplefactory.retrieve());
+    });
+});
