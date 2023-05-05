b126de8f04865ab165198187a271065970e4651e
rainbowginger <rvhj@duck.com>
2023-05-05 19:04:56 +0530

    added test_deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+l;
+// require("@nomiclabs/hardhat-etherscan");
+// require("@nomicfoundation/hardhat-verify");
 
 const RPC_URL = process.env.S_RPC_URL;
 const PVT_KEY = process.env.S_P_KEY;
@@ -15,8 +19,16 @@ module.exports = {
             accounts: [PVT_KEY],
             chainId: CHAIN_ID,
         },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts: [PVT_KEY],
+            chainId: 31337,
+        },
     },
     solidity: "0.8.18",
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 task("accounts", "Prints the list of accounts", async () => {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
     const simplestoragefactory = await ethers.getContractFactory(
@@ -8,6 +8,34 @@ async function main() {
     const deployedStorage = await simplestoragefactory.deploy();
     await deployedStorage.deployed();
     console.log(`deployed to:${deployedStorage.address}`);
+    // console.log(network.config);
+    // if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    //     await deployedStorage.deployTransaction.wait(6);
+    //     await verify(deployedStorage.address, []);
+    // }
+    const StoredValue = await deployedStorage.retrieve();
+    console.log(`Storedvalue: ${StoredValue}`);
+    const Storingvalue = await deployedStorage.store(9);
+    await deployedStorage.deployTransaction.wait(3);
+    console.log(`StoringContract: ${Storingvalue}`);
+    const UpdatedValue = await deployedStorage.retrieve();
+    console.log(`UpdatedValue:${UpdatedValue}`);
+}
+
+async function verify(contractAddress, args) {
+    console.log("verifing contract");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("Already verified")) {
+            console.log("Already verified");
+        } else {
+            console.log(e);
+        }
+    }
 }
 main().catch((error) => {
     console.error(error);
